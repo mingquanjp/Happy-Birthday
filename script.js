5c74bfb9b00ff448d543932524aa3d6f81b25ef6
mingquanjp <faker1652005@gmail.com>
2025-08-11 21:01:15 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const CORRECT_PASSWORD = '123456';
+const CORRECT_PASSWORD = '150805';
 
-let currentPassword = "";
+let currentPassword = '';
 const maxLength = 6;
 
 const passwordDots = document.getElementById('passwordDots');
@@ -11,47 +11,46 @@ const successMessage = document.getElementById('successMessage');
 const errorMessage = document.getElementById('errorMessage');
 const dots = passwordDots.querySelectorAll('.dot');
 
-function updatePasswordDisplay(){
-    dots.forEach((dot, index) => {
-        if ( index < currentPassword.length ) {
-            dot.classList.add('filled');
-        } else {
-            dot.classList.remove('filled');
-        }
-    });
-};
+function updatePasswordDisplay() {
+  dots.forEach((dot, index) => {
+    if (index < currentPassword.length) {
+      dot.classList.add('filled');
+    } else {
+      dot.classList.remove('filled');
+    }
+  });
+}
 
 numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (currentPassword.length < maxLength) {
-            currentPassword += button.textContent;
-            updatePasswordDisplay();
-        }
-    });
+  button.addEventListener('click', () => {
+    if (currentPassword.length < maxLength) {
+      currentPassword += button.textContent;
+      updatePasswordDisplay();
+    }
+  });
 });
 
 clearBtn.addEventListener('click', () => {
-    currentPassword = '';
-    updatePasswordDisplay();
+  currentPassword = '';
+  updatePasswordDisplay();
 });
 
+confirmBtn.addEventListener('click', function (e) {
+  if (currentPassword === CORRECT_PASSWORD) {
+    successMessage.style.display = 'block';
+    errorMessage.style.display = 'none';
+    // Chuyển hướng sang index.html sau khi nhập đúng mật khẩu
+    setTimeout(() => {
+      window.location.href = 'wish.html';
+    }, 500); // Đợi 0.5s để hiển thị thông báo thành công
+  } else {
+    errorMessage.style.display = 'block';
+    successMessage.style.display = 'none';
+    currentPassword = '';
+    updatePasswordDisplay();
 
-confirmBtn.addEventListener('click', function(e){
-    if(currentPassword === CORRECT_PASSWORD) {
-        successMessage.style.display = 'block';
-        errorMessage.style.display = 'none';
-        // Chuyển hướng sang index.html sau khi nhập đúng mật khẩu
-        setTimeout(() => {
-            window.location.href = 'wish.html';
-        }, 500); // Đợi 0.5s để hiển thị thông báo thành công
-    } else {
-        errorMessage.style.display = 'block';
-        successMessage.style.display = 'none';
-        currentPassword = '';
-        updatePasswordDisplay();
-
-        setTimeout(()=> {
-            errorMessage.style.display = 'none';
-        }, 2000);
-    }
+    setTimeout(() => {
+      errorMessage.style.display = 'none';
+    }, 2000);
+  }
 });
